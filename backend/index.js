7be273df21a6fe45bfce96f13cb34ef5cf4a3655
AMAN <nottherealaman@gmail.com>
2022-12-03 20:27:24 +0530

    added collection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -4,43 +4,43 @@ var demo = require('./routes/demo1.js')
 var url = "mongodb://localhost:27017/"
 var obj = require('./data/datas')
 var cors = require('cors')
-
 var MongoClient = require('mongodb').MongoClient
-
 app.use(cors())
+
+
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err
   var dbo = db.db("real-estate")
+  var name= 'Home'
+  dbo.createCollection(name, function (err, res) {
+    if (err) {
+        
+        if (err.codeName =="NamespaceExists") {
+            console.log("Already Exists Collection  : " + name + "")
+            return
+        }
+    }
+    console.log("Collection created! : "+name+"")
 
-  var collectionExists = dbo.ListCollectionNames().ToList().Contains("Home");
-if (collectionExists == false) {
-    // code to create the collection here...
-    dbo.createCollection("Home", function(err, res) {
-      if (err) throw err
-      console.log("Collection created!")
-      
-    })
-  
-  
     dbo.collection('Home').insertMany( obj , (err, res)=>{
       if (err) throw err
       console.log('Documents inserted')
       
     })
 
-    
+})
 
 
-}
-else {
-    Console.log("Collection Already Exists!!")
-}
+
+
 
 })
 
 app.get('/',(req,res)=>{
 
   res.json(obj)
+
+
 })
 app.listen(2000)
